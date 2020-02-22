3b78097b47c2b56556a61e7ec3118a2012592c3d
Ajdin Halac <halac.ajdin@nsoft.com>
2020-02-22 21:18:02 +0100

    Test package

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/AjdinHalac/GoPortScanner/port"
 )
 
 func main() {
 	fmt.Println("Port Scanner in Go")
+
+	open := port.ScanPort("tcp", "localhost", 80)
+	fmt.Printf("Port Open: %t\n", open)
 }
