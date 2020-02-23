c6cc4e3d0ff44a6b36c3581c43c2d3e669334af6
Ajdin Halac <halac.ajdin@nsoft.com>
2020-02-23 11:26:57 +0100

    Add InitialScan for udp

--- main.go
+++ main.go
@@ -9,9 +9,6 @@ import (
 func main() {
 	fmt.Println("Port Scanner in Go")
 
-	open := port.ScanPort("tcp", "localhost", 80)
-	fmt.Printf("Port Open: %t\n", open)
-
 	results := port.InitialScan("localhost")
 	fmt.Println(results)
 }
