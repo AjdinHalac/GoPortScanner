4b2c2df3b24e4ac38b4176209e76165f823940a3
Ajdin Halac <halac.ajdin@nsoft.com>
2020-02-28 16:30:25 +0100

    Use all available procs

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/AjdinHalac/GoPortScanner/port"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	fmt.Println("Port Scanner in Go")
 
 	results := port.InitialScan("localhost")
