4711b4d3980047326f60c302f9bce7b9c8853551
Ajdin Halac <halac.ajdin@nsoft.com>
2020-02-28 15:06:01 +0100

    Split up into goroutines for more parallelism

--- port/port.go
+++ port/port.go
@@ -44,19 +44,21 @@ var KnownTcpPorts = map[int]string{
 	28017: "mongodb web admin [ http://www.mongodb.org/ ]",
 }
 
-const MaxTcpPort = 32728
+const MaxTcpPort = 65535
+const DefaultTimeoutSecs = 5
 
 type ScanResult struct {
-	Port     int
-	Open     bool
-	Protocol string
+	Port        int
+	Open        bool
+	Protocol    string
+	ServiceName string
 }
 
 func ScanTcpPort(hostname string, port int) (result ScanResult) {
 	result := ScanResult{Port: port, Open: false, Protocol: "tcp"}
 
 	address := net.JoinHostPort(hostname, strconv.Itoa(port))
-	conn, err := net.DialTimeout(result.Protocol, address, 60*time.Second)
+	conn, err := net.DialTimeout(result.Protocol, address, DefaultTimeoutSecs*time.Second)
 
 	if err != nil {
 		return
@@ -71,7 +73,7 @@ func ScanUdpPort(hostname string, port int) (result ScanResult) {
 	result := ScanResult{Port: port, Open: false, Protocol: "udp"}
 
 	address := net.JoinHostPort(hostname, strconv.Itoa(port))
-	conn, err := net.DialTimeout(result.Protocol, address, 60*time.Second)
+	conn, err := net.DialTimeout(result.Protocol, address, DefaultTimeoutSecs*time.Second)
 
 	if err != nil {
 		return
@@ -82,7 +84,45 @@ func ScanUdpPort(hostname string, port int) (result ScanResult) {
 	return
 }
 
-func ScanHost(hostname string, lastPort int) (results []ScanResult) {
+func scanMultipleTcpPorts(wg *sync.WaitGroup, ipAddr string, portRange []int) {
+	defer wg.Done()
+	for _, port := range portRange {
+		isOpen := scanTcpPort(ipAddr, port)
+
+		serviceName := KnownTcpPorts[port]
+
+		if isOpen && serviceName != "" {
+			fmt.Println("Target is likely running", KnownTcpPorts[port], "on port", port)
+		} else if isOpen && serviceName == "" {
+			fmt.Println("Port", port, "is open, but no match for known services")
+		}
+	}
+}
+
+func ScanHost(hostname string) (results []ScanResult) {
+	ports = make([]int, len(KnownTcpPorts))
+	curr := 0
+	for k, _ := range KnownTcpPorts {
+		ports[curr] = k
+		curr++
+	}
+	numRoutines = len(ports)
+
+	var wg sync.WaitGroup
+	wg.Add(numRoutines)
+
+	portsPerRoutine := len(ports) / numRoutines
+
+	currStart := 0
+	currEnd := portsPerRoutine + len(ports)%numRoutines
+
+	for currRoutine := 0; currRoutine < numRoutines; currRoutine++ {
+		go scanMultipleTcpPorts(&wg, *targetPtr, ports[currStart:currEnd])
+		currStart = currEnd
+		currEnd += portsPerRoutine
+	}
+
+	wg.Wait()
 	for i := 1; i <= lastPort; i++ {
 		results = append(results, ScanTcpPort(hostname, i))
 	}
@@ -90,6 +130,55 @@ func ScanHost(hostname string, lastPort int) (results []ScanResult) {
 }
 
 func SweepHost(hostname string) (results []ScanResult) {
+	numRoutines = MaxOpenFileDescriptors
+	ports = make([]int, MaxTcpPort)
+	for i := 0; i < MaxTcpPort; i++ {
+		ports[i] = i + 1
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(numRoutines)
+
+	portsPerRoutine := len(ports) / numRoutines
+
+	currStart := 0
+	currEnd := portsPerRoutine + len(ports)%numRoutines
+
+	for currRoutine := 0; currRoutine < numRoutines; currRoutine++ {
+		go scanMultipleTcpPorts(&wg, *targetPtr, ports[currStart:currEnd])
+		currStart = currEnd
+		currEnd += portsPerRoutine
+	}
+
+	wg.Wait()
+	for i := 1; i <= MaxTcpPort; i++ {
+		results = append(results, ScanTcpPort(hostname, i))
+	}
+	return
+}
+
+func SweepHostRange(hostname string, rng int) (results []ScanResult) {
+	numRoutines = MaxOpenFileDescriptors
+	ports = make([]int, rng)
+	for i := 0; i < MaxTcpPort; i++ {
+		ports[i] = i + 1
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(numRoutines)
+
+	portsPerRoutine := len(ports) / numRoutines
+
+	currStart := 0
+	currEnd := portsPerRoutine + len(ports)%numRoutines
+
+	for currRoutine := 0; currRoutine < numRoutines; currRoutine++ {
+		go scanMultipleTcpPorts(&wg, *targetPtr, ports[currStart:currEnd])
+		currStart = currEnd
+		currEnd += portsPerRoutine
+	}
+
+	wg.Wait()
 	for i := 1; i <= MaxTcpPort; i++ {
 		results = append(results, ScanTcpPort(hostname, i))
 	}
